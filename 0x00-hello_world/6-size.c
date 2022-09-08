157e3204f4009227431d703357282d15406f427f
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-08 13:41:43 -0700

    6.size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of char: %li byte(s)\n", sizeof(char));
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %li byte(s)", sizeof(long long int));
+	printf("Size of a long long int: %li byte(s)", sizeof(long long long int));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
