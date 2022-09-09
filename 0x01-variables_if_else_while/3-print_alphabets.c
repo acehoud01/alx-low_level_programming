94ea499eb024adddf4a7185fc4f8f27026bfcc1a
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-09 11:39:15 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x = 'a';
+	char y = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x = x + 1;
+	}
+
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y = y + 1;
+	}
+
+	putchar('\n');
+	return (0);
+}
