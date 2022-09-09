1ba78ee2e52fa18a90fd37a12bcf03b6e845eb02
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-09 12:12:25 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		putchar(48 + j);
+
+		if (j != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+
+		j++;
+	}
+
+	putchar('\n');
+	return (0);
+}
