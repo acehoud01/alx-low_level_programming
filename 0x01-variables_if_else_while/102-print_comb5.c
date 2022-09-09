826865c3043da48e72341569e4d29547406f7416
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-09 14:52:42 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i_one;
+	int i;
+	int j_one;
+	int j;
+
+	for (i_one = 48; i_one < 58; i_one++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 1;
+			j_one = i_one;
+
+			for (; j_one < 58; j_one++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(i_one);
+					putchar(i);
+					putchar(' ');
+					putchar(j_one);
+					putchar(j);
+
+					if (i_one != 57 || j_one != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+
+	putchar('\0');
+	return (0);
+}
