74d405c0e8052ff25ceb700466ed066b2116d358
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-13 13:53:09 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function prototype
+ *
+ * @a: number to add
+ * @b: second number to add
+ * Return: results
+ */
+
+int add(int a, int b)
+{
+	int results = a + b;
+
+	return (results);
+}
