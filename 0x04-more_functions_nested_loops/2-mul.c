4feb87ae832f86f410ddd0e0971c4548e353f428
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-15 04:54:39 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function prototype
+ *
+ * @a: firts number to mul
+ * @b: second number to mul
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
