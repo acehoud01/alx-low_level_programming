ca9bf70174cda0df807a1a937eccdb6ed57c00da
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-15 05:02:41 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prototype
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
