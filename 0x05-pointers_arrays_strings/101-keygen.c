9523d399326430398d6b083b95224074151fab7c
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-19 20:55:27 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+
+	while (j <  2772)
+	{
+		i = rand() % 128;
+
+		if ((j + i) > 2772)
+			break;
+
+		j += i;
+
+		printf("%c", j);
+	}
+	printf("%c\n", (2772 - j));
+
+	return (0);
+}
