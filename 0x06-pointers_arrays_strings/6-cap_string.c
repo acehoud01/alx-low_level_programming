d3665f40ecbbf60cdd308b1ec7b947e369e9e8b0
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-22 10:36:49 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - function prototype
+ *
+ * @str: string to capitalize
+ * Return: 0
+ */
+
+char *cap_string(char *str)
+{
+	char cap[] = ",\t;\n; .!?\"(){}";
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		int j = 0;
+
+		if (i == 0)
+			j = 1;
+		else
+		{
+			int k;
+
+			for (k = 0; cap[k]; k++)
+			{
+				if (str[i - 1] != cap[k])
+					continue;
+				j = 1;
+				break;
+			}
+		}
+
+		if (j != 1)
+			continue;
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= ('a' - 'A');
+	}
+	return (0);
+}
