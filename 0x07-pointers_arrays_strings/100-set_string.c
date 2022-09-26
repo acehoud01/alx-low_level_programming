ca1a68b5c37e1243245a6a4f5b0a2621e99ea750
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-26 13:22:31 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - function prototype
+ *
+ * @s: pointer
+ * @to: value to point to
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
