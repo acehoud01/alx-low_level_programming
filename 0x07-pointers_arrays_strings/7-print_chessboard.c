24693701196690067f24ebfd37532b161697e9cc
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-26 12:57:14 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,16 +9,16 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j;
+	int i;
 
-	for (; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		j = 0;
+		int j;
 
-		for (; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(*(*(i + a) + j));
 		}
-		_putchar('\0');
+		_putchar('\n');
 	}
 }
