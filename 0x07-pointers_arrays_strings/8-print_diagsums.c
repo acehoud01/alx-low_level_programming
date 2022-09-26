f55e9f95cc57cdff7591a82a047f8d95eb2ae764
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-26 13:08:05 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function prototype
+ *
+ * @a: pointer
+ * @size: size of an array
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, k = 0;
+
+	while (i < size)
+	{
+		j = j + a[i];
+		k = k + a[size - 1 - i];
+		a = a + size;
+		i++;
+	}
+
+	printf("%d, ", j);
+	printf("%d\n", k);
+}
