6b91929d74b489f69eeb3fd9b5f79948cf594bef
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-26 12:38:59 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - function prototype
+ * @haystack: to search
+ * @needle: search for
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	for (; haystack[i] > '\0'; i++)
+	{
+		int j = 0;
+
+		for (; haystack[i] > '\0' && needle[j - i] > '\0'; j++)
+		{
+			if (haystack[j] != needle[j - i])
+				break;
+		}
+
+		if (needle[j - i] == '\0')
+			return (haystack + i);
+	}
+
+	return (0);
+}
