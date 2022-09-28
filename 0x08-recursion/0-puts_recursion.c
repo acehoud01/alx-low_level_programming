6b1aa406f9f3f91375f9348dd8e72e9b184f1b11
Anele Bilikwa <aceamahle1@gmail.com>
2022-09-28 03:47:49 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function prototype
+ *
+ * @s: string to be printed
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
