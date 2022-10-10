af2f91fde07faf6fd8c3205d227a9675f12dd6e3
Anele Bilikwa <aceamahle1@gmail.com>
2022-10-10 13:01:41 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include "stdio.h"
+
+/**
+ * print_dog - function prototype
+ *
+ * @d: dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOnwer: %s\n",
+				d->name == NULL ? "(nil)" : d->name,
+				d->age,
+				d->owner == NULL ? "(nil)" : d->owner);
+	}
+}
