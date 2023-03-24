c414a05d4b6767aac9383a5f704e88d469480255
Anele Bilikwa <aceamahle1@gmail.com>
2023-03-24 09:50:36 -0700

    100-operations.so

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -12,3 +12,55 @@ int add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * sub - function that subtract one number
+ * @a: an integer to sub
+ * @b: an int to sub
+ *
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that mutiply two numbers
+ * @a: first product
+ * @b: second product
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function tha divide two numbers
+ * @a: diviser
+ * @b: divisee
+ *
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that check a remainder
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
