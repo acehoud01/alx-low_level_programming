e2b42c8fb4e5f670ed574163b192eb3a43769b4c
Anele Bilikwa <aceamahle1@gmail.com>
2023-03-24 09:26:32 -0700

    100-operations.so

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+/**
+ * add - function that adds two numbers
+ * @a: an integer to add
+ * @b: an integer to add
+ *
+ * Return: a + b
+ */
+
 int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
