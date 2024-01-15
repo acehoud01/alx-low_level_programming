c16e7cbc4722290c24d2dd89594849592398d20f
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-15 20:42:37 +0200

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *argstostr(int ac, char **av) {
+    
+    if (ac == 0 || av == NULL)
+    {
+        return (NULL);
+    }
+
+    
+    size_t total_length = 0;
+    for (int i = 0; i < ac; ++i)
+    {
+        if (av[i] != NULL)
+	{
+            total_length += strlen(av[i]) + 1;
+        }
+    }
+
+    char *result = (char *)malloc((total_length + 1) * sizeof(char));
+
+
+    if (result == NULL)
+    {
+        return (NULL);
+    }
+
+    size_t index = 0;
+    for (int i = 0; i < ac; ++i)
+    {
+        if (av[i] != NULL)
+	{
+            size_t len = strlen(av[i]);
+            strncpy(result + index, av[i], len);
+            index += len;
+            result[index++] = '\n';
+        }
+    }
+
+    result[index] = '\0';
+
+    return (result);
+}
