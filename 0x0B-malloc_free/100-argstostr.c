b1ac7bf2f76e170518cfecb2b7e264ef8875b70a
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-16 05:49:23 +0200

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,35 +11,35 @@
 
 char *argstostr(int ac, char **av)
 {
-	if (ac == 0 || av == NULL)
-	{
-		return (NULL);
-	}
+        if (ac == 0 || av == NULL)
+        {
+                return (NULL);
+        }
 
-	int total_len = 0;
+        int total_len = 0;
 
-	for (int i = 0; i < ac; i++)
-	{
-		total_len += strlen(av[i]) + 1;
-	}
-	total_len++;
+        for (int i = 0; i < ac; i++)
+        {
+                total_len += strlen(av[i]) + 1;
+        }
+        total_len++;
 
-	char *result = (char *)malloc(total_len * sizeof(char));
+        char *result = (char *)malloc(total_len * sizeof(char));
 
-	if (result == NULL)
-	{
-		return (NULL);
-	}
+        if (result == NULL)
+        {
+                return (NULL);
+        }
 
-	char *p = result;
+        char *p = result;
 
-	for (int i = 0; i < ac; i++)
-	{
-		strcpy(p, av[i]);
-		p += strlen(av[i]);
-		*p++ = '\n';
-	}
-	*p = '\0';
+        for (int i = 0; i < ac; i++)
+        {
+                strcpy(p, av[i]);
+                p += strlen(av[i]);
+                *p++ = '\n';
+        }
+        *p = '\0';
 
-	return (result);
+        return (result);
 }
