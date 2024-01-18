492e0471b3ed3e0100734402765812042f5c5a42
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-18 23:51:20 +0200

    101-mul.c correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,78 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-/**
- * multiply_numbers - function
- *
- * @num1: first input
- * @num2: second input
- *
- * Return: num1 * num2
- */
-
-int multiply_numbers(int num1, int num2)
-{
-	return (num1 * num2);
-}
-
-/**
- * is_positive_integer - function
- *
- * @num: input
- * Return: 0
- */
-
-int is_positive_integer(const char *num)
-{
-	while (*num)
-	{
-		if (!isdigit(*num))
-		{
-			return (0);
-		}
-        }
-	num++;
-
-	return (1);
-}
+#include <limits.h>
 
 /**
  * main - entry point
  *
- * @argc: args count
- * @argv: args vector
+ * @argc: number of args
+ * @argv: couting
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
+	unsigned long mul;
+	int i, j;
+
 	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (98);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{ printf("Error\n");
+			exit(98); }
+		}
 	}
-
-	char *endptr;
-	int num1 = strtol(argv[1], &endptr, 10);
-
-	if (*endptr != '\0' || !is_positive_integer(argv[1]))
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	int num2 = strtol(argv[2], &endptr, 10);
-
-	if (*endptr != '\0' || !is_positive_integer(argv[2]))
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	int result = multiply_numbers(num1, num2);
-
-	printf("%d\n", result);
-	return (0);
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
