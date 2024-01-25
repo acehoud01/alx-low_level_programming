68559ec87284abbcc52516dc141bc05776b28ed2
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-01-25 20:55:56 +0200

    complete

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,31 +1,28 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - function prototype
  *
- * @n: the number of arguments
- * Return: sum
+ * @n: numb of args
+ * Return: 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	va_list s;
+	va_list arg_list;
 
 	if (n == 0)
-	{
 		return (0);
-	}
 
-	va_start(s, n);
-
+	va_start(arg_list, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(s, int);
+		sum += va_arg(arg_list, int);
 	}
 
-	va_end(s);
+	va_end(arg_list);
 	return (sum);
 }
