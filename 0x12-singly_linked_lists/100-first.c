e9b17773282426b869257efc4bb6a5b84deab162
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-01 23:10:28 +0200

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * first print before main
  */
 
-__attribute__((constructor)) void my_print_before_main() 
+void __attribute__((constructor)) my_print_before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
