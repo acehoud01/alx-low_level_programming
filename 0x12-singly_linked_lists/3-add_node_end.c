7d22f8cfaee4e3da63c1195a7f81b386ae9ca3a5
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-01 22:39:47 +0200

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * add_node - function prototype
+ *
+ * @head: start
+ * @str: list
+ * Return: 0
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+        list_t *i = malloc(sizeof(list_t));
+
+        if (i == NULL)
+                return (NULL);
+
+        i->str = strdup(str);
+        if (i->str == NULL)
+        {
+                free(i);
+                return (NULL);
+        }
+        i->len = strlen(str);
+        i->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = i;
+	}
+	else
+	{
+		list_t *current = *head;
+
+		while (cuurent-> != NULL)
+		{
+			current = current->next;
+		}
+		current->next = i;
+	}
+
+        return (i);
+}
+
