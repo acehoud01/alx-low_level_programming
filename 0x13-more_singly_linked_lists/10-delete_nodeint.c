e6a1d1dfcbdb6e08d52a2d9f57cfb170a58bc124
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 22:21:22 +0200

    10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - function prototype
+ *
+ * @head: list
+ * @index: postion
+ * Return: 0
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	if (!head)
+		return (-1);
+
+	if (index == 0)
+	{
+		listint_t *temp = *head;
+
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	listint_t *current = *head;
+
+	for (unsigned int i = 0; i < index - 1; i++)
+	{
+		if (current == NULL || current->next == NULL)
+			return (-1);
+
+		current = current->next;
+	}
+
+	if (current->next == NULL)
+		return (-1);
+
+	listint_t *temp = current->next;
+	current->next = temp->next;
+	free(temp);
+	return (1);
+}
