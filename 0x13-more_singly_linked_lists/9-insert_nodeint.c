8485533035f240eb7ca058a6bd0c882e267385c4
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 21:20:35 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *current = *head;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL || head == NULL)
@@ -19,6 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
@@ -27,23 +29,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	listint_t *current = *head;
-	unsigned int count = 0;
-
-	while (current != NULL && count < idx - 1)
+	for (unsigned int i = 0; current && i < idx; i++)
 	{
-		current = current->next;
-		count++;
+		if (i == idx - 1)
+		{
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+		else
+		{
+			current = current->next;
+		}
 	}
 
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+	return (NULL);
 }
