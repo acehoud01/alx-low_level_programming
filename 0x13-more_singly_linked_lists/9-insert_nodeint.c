64c1814f474ef5640a59642008c8221c1777a3ec
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 21:28:04 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,46 +2,41 @@
 
 /**
  * insert_nodeint_at_index - function prototype
- *
- * @head: list
- * @idx: index at given
- * @n: node
- * Return: 0
+ * 
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ * Return: pointer to the new node, or NULL
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *current = *head;
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 
-	if (new_node == NULL || head == NULL)
-	{
-		return (NULL);
-	}
+	if (!new || !head)
+        return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
+
 
 	for (unsigned int i = 0; current && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new_node->next = current->next;
-			current->next = new_node;
-			return (new_node);
+			new->next = current->next;
+			current->next = new;
+			return (new);
 		}
 		else
-		{
 			current = current->next;
-		}
 	}
-
 	return (NULL);
 }
