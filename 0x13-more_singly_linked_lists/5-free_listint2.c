1af4e900bc647b78c720d3161409a275f5cf31ec
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 19:52:36 +0200

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,18 +9,17 @@
 
 void free_listint2(listint_t **head)
 {
-        listint_t *node;
-        listint_t *next_node;
+	listint_t *node;
+	listint_t *next_node;
 
 	if (head == NULL) return;
 
 	node = *head;
-        while (node != NULL)
-        {
-                next_node = node->next;
-                free(node);
-                node = next_node;
+	while (node != NULL)
+	{
+		next_node = node->next;
+		free(node);
+		node = next_node;
         }
-
 	*head = NULL;
 }
