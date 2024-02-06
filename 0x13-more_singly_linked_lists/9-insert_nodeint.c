0a7b09d0e62bd6289347527d0949634f7b64bcfb
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 22:11:03 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * insert_nodeint_at_index - function prototype
- * 
+ *
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
@@ -13,10 +13,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new;
 	listint_t *current = *head;
-	
+
 	new = malloc(sizeof(listint_t));
+	
 	if (!new || !head)
-        return (NULL);
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
