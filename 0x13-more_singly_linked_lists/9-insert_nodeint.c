7076d939cd244cba11b69cc40162f9fa08e3a7d9
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 21:08:44 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,19 +11,16 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	if (head == NULL)
-		return (NULL);
-
 	listint_t *new_node =(listint_t *)malloc(sizeof(listint_t));
 
-	if (new_node == NULL)
+	if (new_node == NULL || new_node == NULL)
 	{
 		return (NULL);
 	}
 
 	new_node->n = n;
 
-	if (idx == 0 || *head == NULL)
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
