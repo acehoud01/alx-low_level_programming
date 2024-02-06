39209b4e1167b17ffceca42b8a3ab1f5fc258c0d
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-06 18:10:20 +0200

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_noteint - function prototype
+ *
+ * @head: head of list
+ * @n: count
+ * Return: 0
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	if (head == NULL)
+		return (NULL);
+
+	listint_t newNode = createNode(n);
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
