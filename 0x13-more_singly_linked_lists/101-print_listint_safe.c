06e5865d2999b0fbfa60ee1d0a15141f42402b45
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-07 05:44:35 +0200

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - function prototype
+ *
+ * @head: list
+ * Return: 0
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	long int def;
+
+	while (head)
+	{
+		def = head - head->next;
+		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
+
+		if (def > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+	return (count);
+}
