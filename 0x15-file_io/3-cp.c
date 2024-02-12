9d6460374c572342be4bfc0ca2364e3f2e1bcdf7
acehoud01 <111900491+acehoud01@users.noreply.github.com>
2024-02-12 22:13:30 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98); }
-		w = write(fd2, buff, size);
+		w = write(fd_to, buff, size);
 		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
