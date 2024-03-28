a1f67de61eae60255b1c715e90bc6eeab2d97318
Anele Bilikwa <aceamahle1@gmail.com>
2024-03-28 10:19:08 -0700

    100-operations.so

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -16,9 +16,12 @@ int multiply(int a, int b)
 	return (a * b);
 }
 
+
 int divide(int a, int b)
 {
-    if (b != 0) 
+    if (b != 0)
+    {
 	    return (a / b);
-    return (-1)
+    }
+    return (-1);
 }
