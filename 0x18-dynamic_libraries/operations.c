7d53f9c5d2b65ddf145136b81a97ebea17e6336b
Anele Bilikwa <aceamahle1@gmail.com>
2024-03-28 10:29:56 -0700

    100-operations.so

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,27 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * add - functio
+ *
+ * @a: first number
+ * @b: sewcond number
+ * Return: 0
+ */
+
 int add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * subtract - function
+ *
+ * @a: first num
+ * @b: second num
+ * Return: 0
+ */
 
 int subtract(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * multiply - function
+ *
+ * @a: 1st num
+ * @b: 2nd num
+ * Return: 0
+ */
+
 int multiply(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * divide - function
+ *
+ * @a: 2st num
+ * @b: 2nd num
+ * Return: 0
+ */
 
 int divide(int a, int b)
 {
-    if (b != 0)
-    {
-	    return (a / b);
-    }
-    return (-1);
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	return (-1);
 }
