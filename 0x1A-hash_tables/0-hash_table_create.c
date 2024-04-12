03aa8ebec423f333190b8a0b0caf04152a5e1985
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-12 20:19:40 +0200

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,14 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-	{
-		return (NULL);
-	}
+		return (NULL)
 
 	for (i = 0; i < size; i++)
-	{
 		new_table->array[i] = NULL;
-	}
 
 	return (new_table);
 }
