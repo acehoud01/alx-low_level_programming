4ba25cb6396439e19cf3866fbc291ef5b72934b4
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-12 19:50:15 +0200

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hast table
+ *
+ * @size: The size of an array
+ * Return: 0
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int i;
+
+	new_table = maalloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
+
+	new_table->size = size;
+	return (new_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+Project Name: Hash Tables 0x1A
+Description
+This project is part of the Data Structures and Algorithms learning series. It focuses on understanding and implementing hash tables. Hash tables are a type of data structure that provide very efficient lookup, insertion, and deletion. In this project, students or users will learn how hash tables work, how to use them, and their applications in solving real-world problems.
